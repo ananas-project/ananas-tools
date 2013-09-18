f7c71c2f1b09863f680783048cc869df5962c8cb
xukun <xukun0217@gmail.com>
2013-09-18 19:00:31 +0800

    add : project "APM".

---
+++ ananas-project-manager/src/ananas/tools/apm/AnanasProjectManager.java
@@ -0,0 +1,12 @@
+package ananas.tools.apm;
+
+public class AnanasProjectManager {
+
+	public static void main(String[] arg) {
+
+		System.out.println("Ananas Project Manager - v0.1");
+		System.out.println("exited!");
+
+	}
+
+}
